904c4ebbcd3c331bc28571a6f0e3c9c8db7ef18f
jose-roberto <jose.junior5@ufv.br>
2024-07-08 21:06:10 -0300

    def connect | update main.py

--- client.py
+++ client.py
@@ -3,5 +3,8 @@ from socket import *
 class Client:
     def __init__(self, username, port):
         self.serverName = username
-        serverPort = port
-        clientSocket = socket(AF_INET, SOCK_STREAM)
+        self.serverPort = port
+        self.clientSocket = socket(AF_INET, SOCK_STREAM)
+
+    def connect(self):
+        self.clientSocket.connect((self.serverName, self.serverPort))
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from server import Server
 from client import Client
 
-INITIAL_PORT = 10000
+INITIAL_PORT = 10001
 CLIENT_COUNT = 0
 
 comands_known = ["/list", "/send", "/help", "/exit"]
+
+server = Server()
 
 while True:
     comand = input("----------- Menu -----------\nEntrar no bate-papo: 1\nSair: 2\n\nDigite um comando: ")
@@ -17,6 +20,10 @@ while True:
         port = INITIAL_PORT + CLIENT_COUNT
 
         client = Client(username, port)
+
+        client.connect()
+
+        comand = input("---------- Opcões ----------\nListar usuários: /list\nEnviar mensagem: /send\nAjuda: /help\nSair: /exit\n\nEscolha uma ação:")
 
     else:
         break
