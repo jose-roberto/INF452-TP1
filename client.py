a4122ec32343d2936fe4c4e14166bab70fa32f2c
jose-roberto <jose.junior5@ufv.br>
2024-07-09 21:53:11 -0300

    delete __pycache__  |  tests in client.py

--- client.py
+++ client.py
@@ -1,52 +1,64 @@
 from socket import *
 import threading
+import select
 
 class Client:
     def __init__(self):
         self.server_ip = "200.235.131.66"
         self.server_port = 10001
+
         self.client_socket = socket(AF_INET, SOCK_STREAM)
-        self.communication_socket = socket(AF_INET, SOCK_STREAM)
+        self.p2p_socket = socket(AF_INET, SOCK_STREAM)
+    
         self.username = ''
 
-    def connect(self):
-        self.client_socket.connect((self.server_ip, self.server_port))
-
-    def create_port(self, user):
-        self.username = user
+    def create_port(self, username):
+        self.username = username
         try:
             _, port = self.client_socket.getsockname()
             self.client_socket.bind(self.client_socket.getsockname())
+            self.client_socket.listen(1)
         except OSError:
             pass
 
         return port
 
+    def connect_to_central_server(self):
+        self.client_socket.connect((self.server_ip, self.server_port))
+
+    def connect_to_p2p(self, ip, port):
+        self.p2p_socket.connect((ip, port))
+        self.send_message(f"USER {self.username}:{port}\r\n") 
+        self.recv_from_server()
+    
+    def check_requests(self):
+        ready_to_read, _, _ = select.select([self.client_socket], [], [], 0.1)
+        if ready_to_read:
+            connectionSocket, addr = self.client_socket.accept()
+
+    def send_user_port(self, username, port):
+        user_port = f"USER {username}:{port}\r\n"
+        self.client_socket.send(user_port.encode())
+
     def keep(self):
         self.client_socket.send("KEEP\r\n".encode())
         threading.Timer(5.0, self.keep).start()
 
-    def send_user_port(self, user_port):
-        self.client_socket.send(user_port.encode())
-
-    def list(self):
+    def get_list(self):
         self.client_socket.send("LIST\r\n".encode())
         self.recv_from_server()
 
-    def address(self, recipient):
+    def get_address(self, recipient):
         self.client_socket.send(f"ADDR {recipient}\r\n".encode())
         addr_message = self.recv_from_server()
 
-        comand, result = (addr_message.decode()).split(' ')
+        _, result = (addr_message.decode()).split(' ')
         ip, port = result.split(':')
 
-        self.communication_socket.connect((ip, int(port)))
-        self.send_message(f"USER {self.username}:{port}\r\n") 
-        self.recv_from_server()       
+        self.connect_to_p2p(ip, int(port))       
 
     def send_message(self, message):
-        self.communication_socket.send(message.encode())
-        # self.recv_from_server()
+        self.p2p_socket.send(message.encode())
 
     def recv_from_server(self):
         self.client_socket.settimeout(5)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from server import Server
 from client import Client
+import threading
 
 INITIAL_PORT = 10001
 
@@ -8,26 +8,34 @@ comands_known = ["/list", "/send", "/help", "/exit"]
 print("Bem-vindo ao chat!")
 
 client = Client()
-client.connect()
+client.connect_to_central_server()
 # client.keep()
 
 username = input("Digite o seu nome de usuário: ")
 port = client.create_port(username)
 
-user_port = f"USER {username}:{port}\r\n"
-client.send_user_port(user_port)
+client.send_user_port(username, port)
+
+def check_requests():
+    while True:
+        client.check_requests()
+
+listener_thread = threading.Thread(target=check_requests)
+listener_thread.daemon = True
+listener_thread.start()
 
 while True: 
     comand = input("\n---------- Menu ----------\nListar usuários: /list\nConectar-se: /addr\nEnviar mensagem: /send\nSair: /exit\n\nEscolha uma ação: ")
     
     if comand == "/list":
-            client.list()
+            client.get_list()
     elif comand == "/addr":
         recipient = input("\nDigite o nome do usuário ao qual deseja-se conectar: ")
-        client.address(recipient)
+        client.get_address(recipient)
     elif comand == "/send":
         client.send_message()
     elif comand == "/exit":
+        # fechar conexões, etc
         break
     else:
         print("\n--- Comando inválido, tente novamente.")
